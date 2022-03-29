61cac465dcc75af5d09cefec6eeb1e8ca6ac3286
drlgfx02xhy <xuhaiyang@mail.ustc.edu.cn>
2022-03-29 18:31:58 +0800

    add small data for svm

--- lab1/util.py
+++ lab1/util.py
@@ -32,7 +32,46 @@ def setup_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
-
+    
+def get_small_data(rate, seed):
+    setup_seed(seed)
+    train_feat = np.load("train_feat.npy")
+    train_label = np.load("train_label.npy")
+    valid_feat = np.load("validation_feat.npy")
+    valid_label = np.load("validation_label.npy")
+    test_feat = np.load("test_feat.npy")
+    test_label = np.load("test_label.npy")
+    
+    train_len = int(len(train_feat) * rate)
+    valid_len = int(len(valid_feat) * rate)
+    test_len = int(len(test_feat) * rate)
+    
+    select_train = random.sample(range(len(train_feat)), train_len)
+    select_valid = random.sample(range(len(valid_feat)), valid_len)
+    select_test = random.sample(range(len(test_feat)), test_len)
+    
+    new_train_feat = train_feat[select_train]
+    new_train_label = train_label[select_train]
+    new_valid_feat = valid_feat[select_valid]
+    new_valid_label = valid_label[select_valid]
+    new_test_feat = test_feat[select_test]
+    new_test_label = test_label[select_test]
+    
+    new_train_feat_path = "small_train_feat" + ".npy"
+    new_train_label_path = "small_train_label" + ".npy"
+    new_valid_feat_path = "small_validation_feat" + ".npy"
+    new_valid_label_path = "small_validation_label" + ".npy"
+    new_test_feat_path = "small_test_feat" + ".npy"
+    new_test_label_path = "small_test_label" + ".npy"
+    
+    np.save(new_train_feat_path, new_train_feat)
+    np.save(new_train_label_path, new_train_label)
+    np.save(new_valid_feat_path, new_valid_feat)
+    np.save(new_valid_label_path, new_valid_label)
+    np.save(new_test_feat_path, new_test_feat)
+    np.save(new_test_label_path, new_test_label)
+    
+    return new_train_feat, new_train_label, new_valid_feat, new_valid_label, new_test_feat, new_test_label
     
     
     
